18fdc3d68cd15e4600de2353c6bb552feda0fccf
David Ford <david@smithandrobot.com>
2014-09-20 22:36:16 -0500

    removing hard coded port 80, heroku handles that. made a change to direcotry var

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.use(compression());
 console.log('Serving from : '+__dirname + '/static')
 app.use(express.static(__dirname + '/static'));
 console.log('Running on port: '+port)
-app.listen(80);
+app.listen(port);
 
 // Add cache headers to express response
 function cacheHeader() {
