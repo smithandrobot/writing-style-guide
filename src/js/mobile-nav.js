6954015aff28d758cf83f971c096ac54ea1fff38
Jonathan Bennett <jonathan@jbennett.me>
2014-09-23 17:43:33 -0400

    menu animations and toggle (WIP)

--- src/js/mobile-nav.js
+++ src/js/mobile-nav.js
@@ -1,11 +1,23 @@
 $(function() {
-  $buttons = $('.mobile-nav__main img');
-  $buttons.on('click', onClick);
-
-  function onClick(e) {
+  var disclosureButtonClicked = function() {
   	var $parent = $(this).parent();
     var $panel = $parent.find('.mobile-nav__main__panel');
     $parent.toggleClass('js-nav-open');
     $panel.slideToggle();
-  }
+  };
+
+  var mainButtonClicked = function() {
+  	var $menu = $(this).parents('.mobile-nav');
+  	var $main = $menu.find('.mobile-nav__main');
+
+  	$menu.toggleClass('js-mobile-nav-open');
+  	$main.slideToggle();
+  };
+
+  $buttons = $('.mobile-nav__main img');
+  $buttons.on('click', disclosureButtonClicked);
+  $toggles = $('.mobile-nav__toggle');
+  $toggles.on('click', mainButtonClicked)
+
+  $('.mobile-nav .mobile-nav__main').slideUp(0); // closed by default
 });
