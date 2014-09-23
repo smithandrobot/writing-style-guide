b9be74ca9211420ad3fce9fdd532e0f6fabbb57a
Jonathan Bennett <jonathan@jbennett.me>
2014-09-23 14:03:04 -0400

    mobile menu updates

--- src/js/mobile-nav.js
+++ src/js/mobile-nav.js
@@ -1,10 +1,9 @@
 $(function() {
-	$buttons = $('.mobile-nav__main img');
+  $buttons = $('.mobile-nav__main img');
   $buttons.on('click', onClick);
 
   function onClick(e) {
     var $panel = $(this).parent().find('.mobile-nav__main__panel');
-    console.log('click', $panel);
-    $panel.css({height:'0px'});
+    $panel.slideToggle();
   }
 });
